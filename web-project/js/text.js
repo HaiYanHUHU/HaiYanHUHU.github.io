4765cb840b70e11f2e3055ec891b539f37f7ef4d
HaiYanHUHU <151022690+HaiYanHUHU@users.noreply.github.com>
2023-12-05 17:42:18 +0000

    Add files via upload
    add web-cake

---
+++ web-project/js/text.js
@@ -0,0 +1,6 @@
+function display(){
+    document.getElementById("cakes-text").style.display = "block";
+}
+function disappear(){
+    document.getElementById("cakes-text").style.display = "none";
+}
